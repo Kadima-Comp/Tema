16d3c05f0e5a02db546d3c6aff02d675b9742f0a
Arthur Oliveira <arthur.ods@outlook.com>
2019-03-29 19:38:18 -0300

    iniciado reestruturação fronend

--- header.php
+++ header.php
@@ -1,49 +1,19 @@
-<html>
-<head lang="pt-br">
-    <meta charset="UTF-8">
-    <?php $home = get_template_directory_uri(); ?>
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?=$home ?>/style.css">
-
-    <title>Tema Kadima</title>
-
-</head>
-<body>
-<!--
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.html">
-        <img src="<?=$home ?>/img/icon.png" width="64" height="64" alt="Logo da Kadima">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle Navigation', 'theme-textdomain' ); ?>">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse flex-grow-1 text-right" id="navbarTogglerDemo02"><!-- AJUSTAR ALINHAMENTO EM XS ->
-        <ul class="navbar-nav ml-auto flex-nowrap">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">Inicio<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Sobre</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Produtos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contato</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Procurar">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Procurar</button>
-        </form>
+    <div class="collapse navbar-collapse" id="navbar-content">
+
+        <?php
+        wp_nav_menu( array(
+            'theme_location' => 'header-menu', // Defined when registering the menu
+            'menu_id'        => 'primary-menu',
+            'container'      => false,
+            'depth'          => 2,
+            'container_class' => 'nav'
+        ) );
+        ?>
     </div>
 </nav>
--->
-<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'navbar navbar-expand-lg navbar-light bg-light') ); ?>
 
--- index.php
+++ index.php
@@ -1,15 +1,33 @@
-<?php get_header(); ?>
+<head lang="pt-br">
+    <meta charset="UTF-8">
+    <?php $home = get_template_directory_uri(); ?>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=$home ?>/style.css">
+
+    <title>Tema Kadima</title>
+</head>
+<body>
 <?php $home = get_template_directory_uri(); ?>
+<?php get_header(); ?>
 <header class="header">
-    <div class="container-fluid"><!-- IMG GRATUITA -->
-        <h1><!--<img src="img/seta.png" height="12%"/>--><?php bloginfo($show = 'name'); ?></h1><h2><?php bloginfo($show = 'description'); ?></h2>
+    <div class="container-fluid">
+        <h1>
+            <?php bloginfo($show = 'name'); ?>
+        </h1>
+        <h2>
+            <?php bloginfo($show = 'description'); ?>
+        </h2>
     </div>
 </header>
-<nav class="navemail"><!-- AJUSTAR POSICIONAMENTO EM XS -->
-    <div class="container">
+<nav class="card w-75 navemail elevation">
+    <!-- AJUSTAR POSICIONAMENTO EM XS -->
+    <div class="card-body">
         <h3>Se cadastre e fique por dentro das novidades!</h3>
         <div class="input-group mb-3">
-            <input type="text" class="capture-email-field form-control" placeholder="E-mail" aria-label="E-mail do usuario" aria-describedby="basic-addon2">
+            <input type="text" class="form-control capture-email-field" placeholder="E-mail" aria-label="E-mail do usuario"
+                aria-describedby="basic-addon2">
             <div class="input-group-append">
                 <button class="capture-email-button btn btn-secondary" type="submit">Enviar</button>
             </div>
@@ -17,88 +35,92 @@
     </div>
 </nav>
 <main class="main">
-    <div class="row" style="width: 100%">
+    <div class="row">
 
         <?php if(is_active_sidebar('sidebar-2')): ?>
-        <aside class="social social-absolute" id="social-bar"><!-- FADE EM ANDAMENTO -->
+        <aside class="social social-absolute" id="social-bar">
+            <!-- FADE EM ANDAMENTO -->
             <ul class="no-margin-padding">
-        <?php dynamic_sidebar( 'sidebar-2' )?>
+                <?php dynamic_sidebar( 'sidebar-2' )?>
             </ul>
         </aside>
         <?php endif; ?>
 
-
-
-
         <div id="art" class="col-xs-12 col-md-9">
             <!-- AQUI CRIA O BOTÃO PARA TIRAR A SELEÇÃO DE CATEGORIA -->
-            <div class="row">
-                <?php
+            <?php
                 if(have_posts()){
-                while(have_posts()){
-                the_post();
-                ?>
-
-                <div class="col-md-12" name="">
-                    <article class="article-main">
+                    while(have_posts()){
+                        the_post();
+                        ?>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
                         <div class="row">
-                            <div class="col-md-4" style="display: block">
-                                <img class="thumbnail" src="<?php
-                                $thumb_id = get_post_thumbnail_id();
-                                $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
-                                echo $thumb_url[0];
-                                ?>">
+                            <div class="col-md-4">
+                                <img class="card-img-top" src="<?php
+                                    $thumb_id = get_post_thumbnail_id();
+                                    $thumb_url = wp_get_attachment_image_src($thumb_id,'thumbnail-size', true);
+                                    echo $thumb_url[0];
+                                    ?>" alt="">
                             </div>
                             <div class="col-md-8">
-                                <a href="#" name="category" onclick="listaCat()">Categoria 1</a><!-- ATUALIZAR 'Categoria 1' COM VARIÁVEL PHP -->
-                                <p class="autor-date">Autor: <?php the_author(); ?> | <?php the_time('d') ?> de <?php the_time('F') ?> de <?php the_time('Y') ?>, às <?php the_time() ?></p>
-                                <h1><?php the_title();?></h1>
-                                <p><?php the_content();?></p>
-                                <a href="<?php the_permalink(); ?>">Leia Mais...</a>
+                                <div class="card-body">
+                                    <a href="#" name="category" class="card-link" onclick="listaCat()">Categoria 1</a><!-- ATUALIZAR 'Categoria 1' COM VARIÁVEL PHP -->
+                                    <p class="card-text">Autor:
+                                        <?php the_author(); ?> |
+                                        <?php the_time('d') ?> de
+                                        <?php the_time('F') ?> de
+                                        <?php the_time('Y') ?>, às
+                                        <?php the_time() ?>
+                                    </p>
+                                    <h4 class="card-title">
+                                        <?php the_title();?>
+                                    </h4>
+                                    <p class="card-text">
+                                        <?php the_content();?>
+                                    </p>
+                                    <a href="<?php the_permalink(); ?>" class="card-link">Leia Mais...</a>
+                                </div>
                             </div>
-
                         </div>
-                    </article>
+                    </div>
                 </div>
-
                 <?php
-                }
-                }
-                ?>
-
-
+    }
+}
+?>
                 <nav aria-label="..." class="pages-group">
                     <ul class="pagination justify-content-center">
-                        <div class="paginacao-main"><?php wordpress_pagination(); ?></div>
+                        <div class="paginacao-main">
+                            <?php wordpress_pagination(); ?>
+                        </div>
 
                     </ul>
                 </nav>
-
-
                 <!--
-
-                 <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item active">
-                            <a class="page-link" href="#">1<span class="sr-only">(current)</span></a></li>
-                        <li class="page-item"><a class="page-link" href="#">2 </a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </li>
-                 -->
-
+<li class="page-item">
+<a class="page-link" href="#" aria-label="Previous">
+<span aria-hidden="true">&laquo;</span>
+<span class="sr-only">Previous</span>
+</a>
+</li>
+<li class="page-item active">
+<a class="page-link" href="#">1<span class="sr-only">(current)</span></a></li>
+<li class="page-item"><a class="page-link" href="#">2 </a>
+</li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>
+<li class="page-item">
+<a class="page-link" href="#" aria-label="Next">
+<span aria-hidden="true">&raquo;</span>
+<span class="sr-only">Next</span>
+</a>
+</li>
+-->
             </div>
         </div>
-        <div class="col-xs-12 col-md-3"><!-- AJUSTAR NO TAMANHO XS -->
+        <div class="col-xs-12 col-md-3">
+            <!-- AJUSTAR NO TAMANHO XS -->
             <div class="row container">
                 <aside class="sidebar">
 
@@ -107,12 +129,13 @@
                     <?php dynamic_sidebar( 'sidebar-1' )?>
                     <?php endif; ?>
 
-
                     <div class="row align-items-center">
-                        <div class="container d-none d-md-block col-md-12"><!-- DESAPARECE EM GRID MENOR QUE MD -->
+                        <div class="container d-none d-md-block col-md-12">
+                            <!-- DESAPARECE EM GRID MENOR QUE MD -->
                             <h4>Se cadastre e fique por dentro das novidades!</h4>
                             <div class="input-group mb-3">
-                                <input type="text" class="capture-email-field form-control" placeholder="E-mail" aria-label="E-mail do usuario" aria-describedby="basic-addon2">
+                                <input type="text" class="capture-email-field form-control" placeholder="E-mail"
+                                    aria-label="E-mail do usuario" aria-describedby="basic-addon2">
                                 <div class="input-group-append">
                                     <button class="capture-email-button btn btn-secondary" type="submit">Enviar</button>
                                 </div>
@@ -126,3 +149,4 @@
     </div>
 </main>
 <?php get_footer(); ?>
+</body>
